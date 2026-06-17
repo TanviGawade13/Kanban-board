0d38461b8d347d00a4dc6f596be0d4c79f303335
BitByBitTanvi <tanvigawade857@gmail.com>
2026-06-17 12:29:45 +0530

    Added card-list skeleton

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 const board = {
-    'title': 'Kanban Board',
-    'board_lists': [
+    title: 'Kanban Board',
+    lists: [
         {
-            'id': 156,
-            'title': 'To do List',
-            'cards':[
+            id: 'list-1',
+            title: 'To Do',
+            cards: [
                 {
-                    'id':123,
-                    'title': 'Do something',
-                    'descp': 'do something there'
+                    id: 'card-1',
+                    title: 'Do something',
+                    description: 'do something there'
                 }
             ]
         },
-        
         {
-            'id': 157,
-            'title': 'In Progress',
-            'cards':[]
+            id: 'list-2',
+            title: 'In Progress',
+            cards: []
         },
-        
         {
-            'id': 158,
-            'title': 'Done',
-            'cards':[]
+            id: 'list-3',
+            title: 'Done',
+            cards: []
         }
     ]
 };
-const list_container = document.querySelector('#lists-container');
-console.log(list_container)
+function renderBoard() {
+    const container = document.getElementById('lists-container');
+    container.innerHTML = "";
+    board.lists.forEach((list) => {
+        const listColumn = document.createElement('div');
+        listColumn.classList.add("list-column");
+        listColumn.dataset.listId = list.id;
+        const listHeader = document.createElement('div');
+        listHeader.classList.add("list-header");
+        const listTitle = document.createElement('span');
+        listTitle.classList.add('list-title');
+        listTitle.textContent = list.title;
+        const cardCount = document.createElement('span');
+        cardCount.classList.add("list-count");
+        cardCount.textContent = list.cards.length;
+        const listDelBtn = document.createElement('button');
+        listDelBtn.classList.add("list-delete-btn");
+        listDelBtn.textContent = "Delete";
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add("cards-container")
+        cardContainer.dataset.listId = list.id;
+        const addCardBtn = document.createElement('button');
+        addCardBtn.textContent = "+ Add a Card";
+        addCardBtn.classList.add("btn-ghost");
+
+
+
+        listColumn.appendChild(listHeader);
+        listColumn.appendChild(cardContainer);
+        listColumn.appendChild(addCardBtn);
+        listHeader.appendChild(listTitle);
+        listHeader.appendChild(cardCount);
+        listHeader.appendChild(listDelBtn);
+        container.appendChild(listColumn);
+    });
+}
+
+renderBoard();
